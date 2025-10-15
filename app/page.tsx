dcb3737872f416fee630aeaac65fe3a22f60f989
Rei <haxhirajrei13@gmail.com>
2025-10-15 14:05:51 +0300

    Added Metadata and Assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <h1 className="h1-bold">Welcome</h1>
+      <h1 className="h1-bold font-inter">Welcome</h1>
+    </>
+  );
 };
 
 export default Home;
